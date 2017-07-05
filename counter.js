68f3ca3be06bb40e223bb81ed07327ad52c49be5
Andrey Arapov <andryhaarapchik@gmail.com>
2017-07-05 19:35:17 +0300

    Первая рабочая версия
    Что делает:
    -ведет подсчет времени от нажатия до нажатия;
    -ведет подсчет нажатий;
    -выводит среднее время (К-фактор);
    -выводит расчетную прибыль;
    -позволяет удалять ошибочные нажатия.
    Что нужно исправить:
    -при изменении параметров, влияющих на прибыль, меняется все проделанные задачи;
    -реализовать онлайн версию.

---
+++ counter.js
@@ -0,0 +1,148 @@
+//просьба не увлек-ся поиском ошибок, ибо работа  дотнетчика)
+
+	var ms = 0;
+	var n = 1;
+	var timerId;
+	var msec = 0;
+	var control = 0;
+	var tempms=0;
+	class Time{
+		constructor(id, description, time)
+		{
+			this.id = id;
+			this.description = description;
+			this.time = time;
+		}
+		GetDescription(){
+			return this.description;
+		}		
+		GetTime(){
+			return this.time;
+		}	
+	}
+	var TimesList = [];
+	function Click()
+	{
+	    	if (control == 0)
+		{		
+			timerId = setInterval(EventTimer, 10);
+			document.getElementById("im").src = "stop.svg";
+			control = 1;
+		}
+		else
+		{
+			document.getElementById("im").src = "play.svg";
+			clearTimeout(timerId);
+			TimesList.push(new Time(n++, document.getElementById("edit").value, ms));
+			control = 0;
+			tempms=msec;
+			WriteColl();
+			ms=0;
+			document.getElementById("nowtime").innerHTML = "0:00:00:000";
+			var z = CalculationSalaries(+document.getElementById("kfref").value, n-1, +document.getElementById("countinpack").value)
+			document.getElementById("zp").innerHTML = z.toFixed(2) + " $";
+		}
+	}
+	function DClick(obj){
+		msec-=+TimesList[obj.id].GetTime();
+		tempms-=+TimesList[obj.id].GetTime();
+		document.getElementById("alltime").innerHTML = ConvertToTimeString(msec);	
+		TimesList.splice(obj.id, 1); //delete record in array
+		n--;
+		ReTable();
+		document.getElementById("kf").innerHTML = parseFloat(((tempms/1000)/(n-1))).toFixed(2);
+		if (n==1) {
+			document.getElementById("count").innerHTML = 0;
+			document.getElementById("kf").innerHTML = 0;
+		}
+		var z = CalculationSalaries(+document.getElementById("kfref").value, n-1, +document.getElementById("countinpack").value)
+		document.getElementById("zp").innerHTML = z.toFixed(2) + " $";
+	}
+
+	function DeleteCell(id){
+		var r = document.getElementById("d"+(+id+2));
+		r.deleteCell(3);
+		r.deleteCell(2);
+		r.deleteCell(1);
+		r.deleteCell(0);
+	}
+	function ReTable(){
+		for (var i = 0; i < n; i++) {
+			DeleteCell(i);
+		}
+		n=1;
+		for (var i = 0; i < TimesList.length; i++) {
+			n++;
+			WriteColl();
+		}
+	}
+	function WriteColl(){
+			var t = document.getElementById("tabledata");
+			var row = t.insertRow(1);
+			row.id="d"+n;
+			var cell = row.insertCell(0);
+			cell.innerHTML = n-1;
+			cell.align="center";
+			cell = row.insertCell(1);
+			cell.innerHTML = TimesList[n-2].GetDescription();
+			cell.align="center";
+			cell = row.insertCell(2);
+			cell.innerHTML = ConvertToTimeString(TimesList[n-2].GetTime());
+			cell.align="center"
+			cell = row.insertCell(3);
+			cell.innerHTML = '<p id ="'+(n-2)+'" onclick="DClick(this)"><img id="im" src="cross.svg" width="20" height="20" /> </p>';
+			document.getElementById("title").innerHTML = n-1;
+			document.getElementById("count").innerHTML = n-1;
+			document.getElementById("kf").innerHTML = parseFloat(((msec/1000)/(n-1))).toFixed(2);
+	}
+	function CalculationSalaries(kfref, countimg, countimginpack){
+		return kfref*countimg*countimginpack*0.06/120;
+	}
+	function ConvertToTimeString(msec){
+		var time = new Date();
+		time.setTime(msec);
+		time.setHours(msec/3600000);
+
+		var stringtime = time.getHours() + ':';	
+		if (time.getMinutes() < 10) {
+			stringtime += "0" + time.getMinutes()+ ':';
+		}
+		else
+		{
+			stringtime += time.getMinutes()+ ':';
+		}
+		
+		if (time.getSeconds() < 10) {
+			stringtime += "0" + time.getSeconds() + ':';
+		}
+		else
+		{
+			stringtime += time.getSeconds() + ':';
+		}
+
+		if (time.getMilliseconds() < 10) {
+			stringtime += "00" + time.getMilliseconds();
+		}
+		else
+		{
+				if (time.getMilliseconds() < 100) 
+				{
+					stringtime += "0" + time.getMilliseconds();
+				}
+				else
+				{
+					stringtime += time.getMilliseconds();
+				}
+		}
+
+		return stringtime;
+	}
+
+	function EventTimer(){
+		ms+=10;
+		msec+=10;
+		var stringtime = ConvertToTimeString(ms);
+			document.getElementById("alltime").innerHTML = ConvertToTimeString(msec);	
+			document.getElementById("nowtime").innerHTML = stringtime;
+	}
+
